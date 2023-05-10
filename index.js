f26525c75c54ecd968cf7a76a46a990675500f17
gouravsahuu <gsgourav.sahu@gmail.com>
2023-05-10 14:05:02 +0530

    updated

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-let resume1 = document.getElementById("resume-button-1");
-let resume2 = document.getElementById("resume-button-2");
+// let resume1 = document.getElementById("resume-button-1");
+// let resume2 = document.getElementById("resume-button-2");
 const home = document.getElementById("home");
 const about = document.getElementById("about");
 const skills = document.getElementById("skills");
@@ -7,18 +7,26 @@ const projects = document.getElementById("projects");
 const contact = document.getElementById("contact");
 const backToTop = document.getElementById("footerBtn");
 
-resume1.addEventListener("click",() => {
-    window.open(
-        "https://drive.google.com/file/d/1EPXtSpyvLBCpQ74wA3rXR4BSPIutNWof/view?usp=share_link",
-        "_blank"
-      );
-})
-resume2.addEventListener("click",() => {
-    window.open(
-        "https://drive.google.com/file/d/1EPXtSpyvLBCpQ74wA3rXR4BSPIutNWof/view?usp=share_link",
-        "_blank"
-      );
-})
+document.getElementById('resume-link-1').addEventListener("click",()=>{
+    window.location.assign("https://drive.google.com/file/d/1EPXtSpyvLBCpQ74wA3rXR4BSPIutNWof/view?usp=share_link", target="_blank");
+  })
+  
+  document.getElementById('resume-link-2').addEventListener("click",()=>{
+    window.location.assign("https://drive.google.com/file/d/1EPXtSpyvLBCpQ74wA3rXR4BSPIutNWof/view?usp=share_link", target="_blank");
+  })
+
+// resume1.addEventListener("click",() => {
+//     window.open(
+//         "https://drive.google.com/file/d/1EPXtSpyvLBCpQ74wA3rXR4BSPIutNWof/view?usp=share_link",
+//         "_blank"
+//       );
+// })
+// resume2.addEventListener("click",() => {
+//     window.open(
+//         "https://drive.google.com/file/d/1EPXtSpyvLBCpQ74wA3rXR4BSPIutNWof/view?usp=share_link",
+//         "_blank"
+//       );
+// })
 
 home.addEventListener("click",() => {
   const element = document.getElementsByClassName("home")[0];
