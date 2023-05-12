cb51f1a018d48a4bff83cf11802b234f53e229d4
gouravsahuu <gsgourav.sahu@gmail.com>
2023-05-12 12:21:52 +0530

    added responsiveness

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-// let resume1 = document.getElementById("resume-button-1");
-// let resume2 = document.getElementById("resume-button-2");
+let resume1 = document.getElementById("resume-button-1");
+let resume2 = document.getElementById("resume-button-2");
 const home = document.getElementById("home");
 const about = document.getElementById("about");
 const skills = document.getElementById("skills");
@@ -7,26 +7,21 @@ const projects = document.getElementById("projects");
 const contact = document.getElementById("contact");
 const backToTop = document.getElementById("footerBtn");
 
-document.getElementById('resume-link-1').addEventListener("click",()=>{
-    window.location.assign("https://drive.google.com/file/d/1EPXtSpyvLBCpQ74wA3rXR4BSPIutNWof/view?usp=share_link", target="_blank");
-  })
-  
-  document.getElementById('resume-link-2').addEventListener("click",()=>{
-    window.location.assign("https://drive.google.com/file/d/1EPXtSpyvLBCpQ74wA3rXR4BSPIutNWof/view?usp=share_link", target="_blank");
-  })
 
-// resume1.addEventListener("click",() => {
-//     window.open(
-//         "https://drive.google.com/file/d/1EPXtSpyvLBCpQ74wA3rXR4BSPIutNWof/view?usp=share_link",
-//         "_blank"
-//       );
-// })
-// resume2.addEventListener("click",() => {
-//     window.open(
-//         "https://drive.google.com/file/d/1EPXtSpyvLBCpQ74wA3rXR4BSPIutNWof/view?usp=share_link",
-//         "_blank"
-//       );
-// })
+
+
+resume1.addEventListener("click",() => {
+    window.open(
+        "https://drive.google.com/file/d/1EPXtSpyvLBCpQ74wA3rXR4BSPIutNWof/view?usp=share_link",
+        "_blank"
+      );
+})
+resume2.addEventListener("click",() => {
+    window.open(
+        "https://drive.google.com/file/d/1EPXtSpyvLBCpQ74wA3rXR4BSPIutNWof/view?usp=share_link",
+        "_blank"
+      );
+})
 
 home.addEventListener("click",() => {
   const element = document.getElementsByClassName("home")[0];
@@ -52,3 +47,29 @@ footerBtn.addEventListener("click",() => {
     const element = document.getElementsByClassName("home")[0];
     element.scrollIntoView({ behavior: 'smooth' });
   })
+
+// hamburger 
+const ham = document.querySelector(".hamburger");
+const navMenu = document.getElementById("nav-main");
+
+ham.addEventListener("click",() => {
+    ham.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+
+window.onscroll = () => {
+    ham.classList.remove("active");
+    navMenu.classList.remove("active");
+}
+
+document.querySelectorAll(".link-nav").forEach(n => n.addEventListener("click", () => {
+    ham.classList.remove("active");
+    navMenu.classList.remove("active");
+}))
+
+document.querySelectorAll(".home-git").forEach(n => n.addEventListener("click", () => {
+    window.open("https://github.com/gouravsahuu","_blank");
+}))
+document.querySelectorAll(".home-linkedIn").forEach(n => n.addEventListener("click", () => {
+    window.open("https://www.linkedin.com/in/gouravsahuu/","_blank");
+}))
